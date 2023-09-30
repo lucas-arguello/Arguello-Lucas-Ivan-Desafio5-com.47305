dc5666cebfeb94532145aa1e175ffd213792bf22
Lucas Arguello <lucas5ivan@hotmail.com>
2023-09-29 22:08:44 -0300

    creando funciones del manager de carritos

--- e-commerce/src/dao/mongo/cartsManagerMongo.js
+++ e-commerce/src/dao/mongo/cartsManagerMongo.js
@@ -4,13 +4,65 @@ export class CartsManagerMongo{
             constructor() {
                 this.model = cartsModel;
             };
+            //Aca creo el carrito
+            async createCarts (productId) {
+                try{
+                    const cart = await this.model.create({ product: [{ productId, quantity: 1 }] });
 
-            async createCarts ()  {};
-            async getCarts () {};
+                    return cart
 
+                }catch(error){
+                    console.log("createCarts",error.message);
+                    throw new Error("No se pudo crear el carrito");
 
+                };
+                
+            };
 
+            //Aca obtengo el listado de carritos
+            async getCarts () {
+                try {
 
+                    const carts = await this.model.find();
+
+                    return carts
+
+                }catch(error){
+                    console.log("getCarts",error.message);
+                    throw new Error("No se pudieron obtener el listado de carritos");
+
+                };
+            };
+
+            //Aca agragmos productos al carrito
+            async addProduct(cartId, productId) {
+                try {
+                    const cart = await this.model.findById(cartId);
+                    if (!cart) {
+                      throw new Error("Carrito no encontrado");
+                    }
+              
+                    // Verifica si el producto ya existe en el carrito
+                    const existingProduct = cart.product.find((item) => item.productId === productId);
+
+                    if (existingProduct) {
+                        // Incrementa la cantidad si el producto ya existe
+                        existingProduct.quantity += quantity || 1; 
+                    } else {
+                        // Agrega un nuevo producto al carrito
+                        cart.product.push({ productId, quantity: quantity || 1 });
+                    }
+                    await cart.save();
+              
+                    return cart;
+
+                }catch(error){
+                    console.log("addProduct",error.message);
+                    throw new Error("No se pudieron agregar productos al carrito");
+
+                };
+
+            };
 
 
 } 
